f552a6a5ff0f5991a846b472e43986dcd3f98d59
patricksevat <patrick.sevat@rabobank.nl>
2021-08-13 17:51:23 +0200

    feat(): add keyboard navigation

--- script.js
+++ script.js
@@ -1,28 +1,63 @@
-(function () {
-  addClickListenersToMenuItems();
-  // normally we would clean up any event listeners to prevent mem leaks
-  // but closing the page will take care of that for us
-})();
+const selectors = {
+  menuItemText: '.menu-item__text',
+  subMenu: '.menu-item__submenu',
+  subMenuItem: '.menu-item__submenu-item',
+}
+
+function addKeyboardEventListeners() {
+  document.addEventListener('keyup', function (keyboardEvent) {
+    if(keyboardEvent.key === 'Enter' || keyboardEvent.code === 'Space') {
+      const activeElement = document.activeElement;
+      if(activeElement.getAttribute('tabindex') === '0') {
+        activeElement.dispatchEvent(new Event('click'));
+      }
+    }
+  })
+}
+
+function addClickListenersToSubMenuItems () {
+  const subMenuItems = document.querySelectorAll(selectors.subMenuItem);
+  subMenuItems.forEach(subMenuItem => {
+    subMenuItem.addEventListener('click', alertOnSubMenuItemClick)
+  })
+}
+
+function alertOnSubMenuItemClick (clickEvent) {
+  const subMenuItem = clickEvent.target;
+  alert(subMenuItem.innerText);
+}
+function addClickListenersToMenuItems() {
+  const menuItems = document.querySelectorAll(selectors.menuItemText);
+  menuItems.forEach(menuItem => {
+    menuItem.addEventListener('click', toggleSubmenu);
+  })
+}
 
 function toggleSubmenu (clickEvent) {
   const spanElement = clickEvent.target;
   const menuItem = spanElement.parentElement;
-  const subMenu = menuItem.querySelector('.menu-item__submenu');
+  const subMenu = menuItem.querySelector(selectors.subMenu);
 
   if(subMenu.hasAttribute('hidden')) {
+    // TODO close open submenus
     subMenu.removeAttribute('hidden');
     subMenu.removeAttribute('aria-hidden');
+    addSubMenuItemTabIndices(subMenu);
   } else {
     subMenu.setAttribute('hidden', '');
     subMenu.setAttribute('aria-hidden', 'true');
+    removeSubMenuItemTabIndices(subMenu);
   }
 }
 
-function addClickListenersToMenuItems() {
-  const menuItems = document.querySelectorAll('.menu-item__text');
-  menuItems.forEach(menuItem => {
-    menuItem.addEventListener('click', toggleSubmenu);
-  })
+function removeSubMenuItemTabIndices (htmlElement) {
+  const subMenuItems = htmlElement.querySelectorAll(selectors.subMenuItem);
+  subMenuItems.forEach((subMenuItem) => subMenuItem.setAttribute('tabindex', '-1'));
+}
+
+function addSubMenuItemTabIndices (htmlElement) {
+  const subMenuItems = htmlElement.querySelectorAll(selectors.subMenuItem);
+  subMenuItems.forEach((subMenuItem) => subMenuItem.setAttribute('tabindex', '0'));
 }
 
 function toggleCssTheme (newTheme) {
@@ -41,3 +76,10 @@ function toggleCssTheme (newTheme) {
   }
 }
 
+(function () {
+  addKeyboardEventListeners();
+  addClickListenersToSubMenuItems();
+  addClickListenersToMenuItems();
+  // normally we would clean up any event listeners to prevent mem leaks
+  // but closing the page will take care of that for us
+})();
